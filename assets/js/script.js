dd33a3e57f278c107c82ae35e26dc010a91f91ad
raigonlab <rrvnna@gmail.com>
2026-05-18 16:31:48 +0200

    Add division code

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,6 +44,9 @@ function runGame(gameType) {
     } else if (gameType === "subtract") {
         displaySubtractQuestion(num1, num2);
 
+    } else if (gameType === "division") {
+        displayDivisionQuestion(num1, num2);
+
 
     } else { 
         alert(`Unknow game type: ${gameType}`);
@@ -91,6 +94,9 @@ function calculateCorrectAnswer () {
     }else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
 
+    }else if (operator === "/") {
+        return [operand1 / operand2, "division"];
+
     }else { alert(`Unimplemented operator ${operator}`);
     throw `Unimplemented operator ${operator}. Aborting!`;
 }
@@ -122,7 +128,7 @@ function displayAdditionQuestion (operand1, operand2) {
 
 function displaySubtractQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
-    document.getElementById('operand2').textContent = operand1 > operand2 ? operand1 : operand1;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand1 : operand2;
     document.getElementById('operator').textContent = "-";
 
 }
@@ -134,5 +140,10 @@ function displayMultiplyQuestion (operand1, operand2) {
 
 }
 
+function displayDivisionQuestion (operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1 * operand2;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "/";
 
+}
 
